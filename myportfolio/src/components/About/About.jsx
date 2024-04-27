c0e84c6cfeb61f8e176d7be6231142589eb8b13d
manish5124 <manishchouhan5124@gmail.com>
2024-04-27 22:59:19 +0530

    about fixed

--- myportfolio/src/components/About/About.jsx
+++ myportfolio/src/components/About/About.jsx
@@ -7,25 +7,20 @@ function About() {
         <section id="about" className="sections">
         <h2 className="magnify_text" data-aos-duration="1000" data-aos="fade-up">About Me</h2>
       <p className="text-secondary story-content data">
-        {`Hello, I'm Manish Chouhan, a dedicated full-stack web developer with a penchant for crafting robust digital solutions. With a fervent passion for coding, I immerse myself in the dynamic realms of Java for backend development and harness the power of React for front-end mastery.
-`}
-        <br />
-        <br />
-        {`
+      <span>Hello, I'm Manish Chouhan, a dedicated full-stack web developer with a penchant for crafting robust digital solutions. With a fervent passion for coding, I immerse myself in the dynamic realms of Java for backend development and harness the power of React for front-end mastery.</span>
+  
+       <span className="notImp">
       Currently, I thrive as a junior software developer at Cognizant, a prominent America-based multinational corporation, where I've honed my skills over the past two years. Within this stimulating environment, I've encountered numerous challenges along my developmental journey. Yet, armed with resilience and a collaborative spirit, I've worked alongside my esteemed team to surmount obstacles and deliver exceptional value to our clientele, consistently meeting deadlines with finesse.
-      `}
-        <br />
-        <br />
-        {` Recognized by both my peers and the organization for my contributions to team synergy and project success, I find immense fulfillment in the collective achievements we've orchestrated. Beyond the lines of code, I embody the ethos of a team player, fostering an environment where innovation thrives and excellence becomes the norm.`}
-        <br />
-        <br />
-        {`Driven by a relentless pursuit of excellence, I remain steadfast in my commitment to pushing the boundaries of technological innovation, continuously seeking opportunities to refine my craft and elevate the digital landscape. With an unwavering dedication to excellence and a fervent love for coding, I embark on each new project with enthusiasm, eager to leave an indelible mark on the digital frontier.`}
+      </span>
+  
+        <span className="notImp"> Recognized by both my peers and the organization for my contributions to team synergy and project success, I find immense fulfillment in the collective achievements we've orchestrated. Beyond the lines of code, I embody the ethos of a team player, fostering an environment where innovation thrives and excellence becomes the norm.
+        </span>
+   
+        <span>Driven by a relentless pursuit of excellence, I remain steadfast in my commitment to pushing the boundaries of technological innovation, continuously seeking opportunities to refine my craft and elevate the digital landscape. With an unwavering dedication to excellence and a fervent love for coding, I embark on each new project with enthusiasm, eager to leave an indelible mark on the digital frontier. </span>
       </p>
       <div className="button">
-        <button type="button" class="btn">
-          VIEW RESUME
-        </button>
-      </div>
+          <a href="https://resume.smithgajjar.dev" target="_blank" className="btn">VIEW RESUME</a>
+        </div>
       </section>
     </div>
   );
